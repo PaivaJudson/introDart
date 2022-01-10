678e1dcebbf0881d280dd39592ca5f3a8e3f2a8b
Judson Paiva <judson.paiva@hotmail.com>
2022-01-10 20:15:14 +0100

    first commit

---
+++ hello.dart
@@ -0,0 +1,19 @@
+
+main(){
+
+  print(calcAreaCirculo(5));
+
+}
+
+void imprimirInfo(){
+  print("Seja bem-vindo(a)!");
+  print("Ao mundo do Dart!");
+}
+
+double calcularSoma(double a, double b){
+  return a + b;
+}
+
+double calcAreaCirculo(double raio) => 3.14 * raio * raio;
+
+
